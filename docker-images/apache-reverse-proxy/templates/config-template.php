0dbac474d0e7f5bc9b79ef8497836b07ba964156
unknown <batach31@hotmail.com>
2019-06-09 21:28:57 +0200

    bonus 6

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,14 +1,33 @@
 <?php
-	$DYNAMIC_APP = getenv('DYNAMIC_APP');
-	$STATIC_APP = getenv('STATIC_APP');
+// on en met une de chaque pour montrer le load-balancing
+	$DYNAMIC_APP_A = getenv('DYNAMIC_APP_A');
+	$DYNAMIC_APP_B = getenv('DYNAMIC_APP_B');
+	$STATIC_APP_A = getenv('STATIC_APP_A');
+	$STATIC_APP_B = getenv('STATIC_APP_B');
+
 ?>
 
 <VirtualHost *:80>
     ServerName demo.res.ch
+	
+	<Location "/balancer-manager">
+		SetHandler balancer-manager
+		//Require host example.com pas besoin ici
+	</Location>
 
-    ProxyPass "/api/adresses/" "http://<?php print $DYNAMIC_APP ?>/"
-    ProxyPassReverse "/api/adresses/" "http://<?php print $DYNAMIC_APP ?>/"
+    <Proxy "balancer://dynamic-cluster">
+        BalancerMember 'http://<?php print "$DYNAMIC_APP_A"?>/'
+        BalancerMember 'http://<?php print "$DYNAMIC_APP_B"?>/'
+    </Proxy>
+	
+    ProxyPass "/api/adresses/" "balancer://dynamic-cluster/"
+    ProxyPassReverse "/api/adresses/" "balancer://dynamic-cluster/"
+	
+	<Proxy "balancer://static-cluster">
+        BalancerMember 'http://<?php print "$STATIC_APP_A"?>/'
+        BalancerMember 'http://<?php print "$STATIC_APP_B"?>/'
+    </Proxy>
     
-    ProxyPass "/" "http://<?php print $STATIC_APP ?>/"
-    ProxyPassReverse "/" "http://<?php print $STATIC_APP ?>/"
+    ProxyPass "/" "balancer://static-cluster/"
+    ProxyPassReverse "/" "balancer://static-cluster/"
 </VirtualHost>
